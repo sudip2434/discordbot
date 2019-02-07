9bad54a776bb2f2dec5163c4b88277dc8f23c12e
sudip2434 <basak.sudipto.97@gmail.com>
2019-02-07 19:02:33 +0530

    intermediate changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord. Client();
-const token = require('./config.json');
+const token = require('./conf.json');
 
 client.login(token.token);
 // console.log(token)
